dfc26faf79d0d5103f9583b9414a41a25ddeb88f
Rustam Sharshenov <rustam@sharshenov.com>
2015-03-29 17:36:29 +0600

    Create payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,22 @@ class PaymentsController < ApplicationController
   before_action :authenticate_organization!
 
   def new
+    @payment = Payment.new
   end
+
+  def create
+    @payment = current_organization.payments.create! payment_params
+    redirect_to @payment
+  end
+
+  def show
+    @payment = current_organization.payments.find params[:id]
+  end
+
+  private
+
+    def payment_params
+      params.require(:payment).permit :amount
+    end
 
 end
